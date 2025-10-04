b77b35ddb99da7dd9e036a09a5a4a886c1d33ca2
Kirlian Kitzinger <kirliankitzinger@gmail.com>
2025-10-04 20:33:13 -0300

    Add files via upload

--- portfoliodjango/portfolio_project/settings.py
+++ portfoliodjango/portfolio_project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "portfolio_app"
+    'whitenoise.runserver_nostatic',
     ]
 
 MIDDLEWARE = [
@@ -125,8 +126,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-
-
-
