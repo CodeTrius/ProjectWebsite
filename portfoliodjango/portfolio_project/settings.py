a62000ad9c145b1813c09f22541b9119f18ec5c6
Kirlian Kitzinger <kirliankitzinger@gmail.com>
2025-10-04 18:56:57 -0300

    Update settings.py

--- portfoliodjango/portfolio_project/settings.py
+++ portfoliodjango/portfolio_project/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise',
 ]
 
 ROOT_URLCONF = 'portfolio_project.urls'
@@ -121,3 +122,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
